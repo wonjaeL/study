e303059082b5e6d0c560bbbf6245e049b830a006
gyusika <gyusika@naver.com>
2020-01-30 21:59:26 +0900

    Hello

--- assignment.py
+++
@@ -1,6 +0,0 @@
-a = 1
-
-for i in range(30):
-    a *= 2
-
-print(a)
---
+++ assignment2.py
@@ -0,0 +1,10 @@
+music_chart = [
+    {'name': 'Bang Bang Bang', 'singer': 'bigbang', 'count': 333},
+    {'name': 'aaaaa', 'singer': 'bigbang', 'count': 444},
+    {'name': 'ffffff', 'singer': 'wonjae', 'count': 111},
+    {'name': 'ccccc', 'singer': 'wonjae', 'count': 555},
+    {'name': 'bbbbbbb', 'singer': 'wonjae', 'count': 111},
+    {'name': 'eeeeeeeee', 'singer': 'wonjae', 'count': 111},
+]
+
+temp = {}
