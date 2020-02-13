0fb5696522fe0ff37a85e63ce465f8804099b45c
potato <dkxha24@naver.com>
2020-02-13 19:52:44 +0900

    add phase03 sample

---
+++ phase03/sample.py
@@ -0,0 +1,57 @@
+music_chart = [
+    {'name': 'Bang Bang Bang', 'singer': 'bigbang', 'count': 333},
+    {'name': 'aaaaa', 'singer': 'bigbang', 'count': 444},
+    {'name': 'ffffff', 'singer': 'wonjae', 'count': 111},
+    {'name': 'ccccc', 'singer': 'wonjae', 'count': 555},
+    {'name': 'bbbbbbb', 'singer': 'wonjae', 'count': 111},
+    {'name': 'eeeeeeeee', 'singer': 'wonjae', 'count': 111},
+]
+
+temp_count_dict = {}
+
+for music in music_chart:
+    singer = music['singer']
+    if singer not in temp_count_dict:
+        temp_count_dict[singer] = 0
+
+    count_ = temp_count_dict[singer] + music['count']
+    temp_count_dict[singer] = count_
+
+print(temp_count_dict['bigbang'])
+print(temp_count_dict['wonjae'])
+for i in range(0, 4):
+ print(singer_list[i])
+ print(temp_count_dict_Like[singer_list[i]])
+ print(temp_count_dict_profit[singer_list[i]])
+
+print(singer_list[0])
+print(temp_count_dict_Like[singer_list[0]])
+print(temp_count_dict_profit[singer_list[0]])
+
+print(singer_list[1])
+print(temp_count_dict_Like[singer_list[1]])
+print(temp_count_dict_profit[singer_list[1]])
+
+print(singer_list[2])
+print(temp_count_dict_Like[singer_list[2]])
+print(temp_count_dict_profit[singer_list[2]])
+
+print(singer_list[3])
+print(temp_count_dict_Like[singer_list[3]])
+print(temp_count_dict_profit[singer_list[3]])
+
+print(singer_list[4])
+print(temp_count_dict_Like[singer_list[4]])
+print(temp_count_dict_profit[singer_list[4]])
+
+best_song = {}
+
+for music in music_list:
+    music = music['music_name']
+
+    best_like=
+    best_song = music['music_name']
+    best_song['like'] =
+
+best_song= print(best_song['like'])
+
