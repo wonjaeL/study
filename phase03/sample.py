80d983c2abc571bf254b82431c989c850e90c475
Dong Hyun <gimdh1218@naver.com>
2020-02-13 20:12:04 +0900

    my work

--- phase03/sample.py
+++ phase03/sample.py
@@ -2,28 +2,28 @@ class Animal:
     def __init__(self, name, age, leg_count):
         pass
 
-    ## 다리개수를 출력하는 함수구현
+    ## 다리개수를 출력하는 함수 구현
     def print_legs(self):
-        pass
+        print(self.leg_count)
 
 
-class Dog:
+class Dog(Animal):
     ## 이름과 나이를 출력하는 함수 구현
     def name_age(self):
-        pass
+        print("Name = ", self.name, "\nAge = ", self.age)
 
 
-class Cat:
+class Cat(Animal):
     ## 나이 곱하기 다리개수를 출력하는 함수 구현
     def age_leg(self):
-        pass
+        print(self.age * self.leg_count)
 
 
-dog1 = Dog()
-dog2 = Dog()
-cat1 = Cat()
-cat2 = Cat()
-cat3 = Cat()
+dog1 = Dog("James", 3, 4)
+dog2 = Dog("Bong", 10, 5)
+cat1 = Cat("Rilly", 2, 4)
+cat2 = Cat("Jason", 5, 3)
+cat3 = Cat("Bro", 6, 4)
 
 animals = [dog1, dog2, cat1, cat2, cat3]
 
