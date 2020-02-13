8eae55b3b32bee8edc9b82a9e3da18f2db4556b9
potato <dkxha24@naver.com>
2020-02-13 20:23:09 +0900

    animal

--- phase03/sample.py
+++ phase03/sample.py
@@ -1,23 +1,29 @@
 class Animal:
     def __init__(self, name, age, leg_count):
+        self.name = name
+        self.age = age
+        self.leg_count = leg_count
         pass
 
 
     ## 다리개수를 출력하는 함수구현
     def print_legs(self):
-
+        print(self.leg_count)
         pass
 
 
 class Dog(Animal):
     ## 이름과 나이를 출력하는 함수 구현
     def name_age(self):
+        print(self.name)
+        print(self.age)
         pass
 
 
 class Cat(Animal):
     ## 나이 곱하기 다리개수를 출력하는 함수 구현
     def age_leg(self):
+        print(self.leg_count*self.age)
         pass
 
 
