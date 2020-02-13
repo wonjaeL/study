9dab8fa43b6117769aae96a4df0dab7787146a28
potato <dkxha24@naver.com>
2020-02-13 19:53:30 +0900

    add phase03 sample

--- phase03/sample.py
+++ phase03/sample.py
@@ -1,57 +1,35 @@
-music_chart = [
-    {'name': 'Bang Bang Bang', 'singer': 'bigbang', 'count': 333},
-    {'name': 'aaaaa', 'singer': 'bigbang', 'count': 444},
-    {'name': 'ffffff', 'singer': 'wonjae', 'count': 111},
-    {'name': 'ccccc', 'singer': 'wonjae', 'count': 555},
-    {'name': 'bbbbbbb', 'singer': 'wonjae', 'count': 111},
-    {'name': 'eeeeeeeee', 'singer': 'wonjae', 'count': 111},
-]
+class Animal:
+    def __init__(self, name, age, leg_count):
+        pass
 
-temp_count_dict = {}
+    ## 다리개수를 출력하는 함수구현
+    def print_legs(self):
+        pass
 
-for music in music_chart:
-    singer = music['singer']
-    if singer not in temp_count_dict:
-        temp_count_dict[singer] = 0
 
-    count_ = temp_count_dict[singer] + music['count']
-    temp_count_dict[singer] = count_
+class Dog:
+    ## 이름과 나이를 출력하는 함수 구현
+    def name_age(self):
+        pass
 
-print(temp_count_dict['bigbang'])
-print(temp_count_dict['wonjae'])
-for i in range(0, 4):
- print(singer_list[i])
- print(temp_count_dict_Like[singer_list[i]])
- print(temp_count_dict_profit[singer_list[i]])
 
-print(singer_list[0])
-print(temp_count_dict_Like[singer_list[0]])
-print(temp_count_dict_profit[singer_list[0]])
+class Cat:
+    ## 나이 곱하기 다리개수를 출력하는 함수 구현
+    def age_leg(self):
+        pass
 
-print(singer_list[1])
-print(temp_count_dict_Like[singer_list[1]])
-print(temp_count_dict_profit[singer_list[1]])
 
-print(singer_list[2])
-print(temp_count_dict_Like[singer_list[2]])
-print(temp_count_dict_profit[singer_list[2]])
+dog1 = Dog()
+dog2 = Dog()
+cat1 = Cat()
+cat2 = Cat()
+cat3 = Cat()
 
-print(singer_list[3])
-print(temp_count_dict_Like[singer_list[3]])
-print(temp_count_dict_profit[singer_list[3]])
+animals = [dog1, dog2, cat1, cat2, cat3]
 
-print(singer_list[4])
-print(temp_count_dict_Like[singer_list[4]])
-print(temp_count_dict_profit[singer_list[4]])
-
-best_song = {}
-
-for music in music_list:
-    music = music['music_name']
-
-    best_like=
-    best_song = music['music_name']
-    best_song['like'] =
-
-best_song= print(best_song['like'])
-
+for animal in animals:
+    if type(animal) == Dog:
+        animal.name_age()
+    if type(animal) == Cat:
+        animal.age_leg()
+    animal.print_legs()
