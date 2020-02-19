950c565dda444eba24249da624b6736c75778ea8
Dong Hyun <gimdh1218@naver.com>
2020-02-19 14:34:50 +0900

    I did my best but...

--- phase03_gwajay/answer.py
+++ phase03_gwajay/answer.py
@@ -2,24 +2,35 @@ from phase03_gwajay.moonjae import get_moonjae  # Do not change this line
 
 
 class Music:
+    def __init__(self, name, like, album_price):
+        self.name = name
+        self.like = like
+        self.album_price = album_price
     # name 이라는 attribute를 추가해주세요
     # like라는 attribute를 추가해주세요
     # album_price라는 attribute를 추가해주세요
-    pass
+
 
 
 class Singer:
+    def __init__(self, name):
+        self.name = name
+    music_list = []
+    for music_list_dict in music_list:
+        if music_list_dict['singer_name'] == self.name:
+            music_list.append(music_list_dict['music_name'])
     # name 이라는 attribute를 추가해주세요
     # music_list 라는 list 를 만들어서 가수가 부른 노래를 저장해주세요
 
     def get_song_list(self):
         return self.music_list
 
-    pass
+
 
 
 def answer():
     singer_list = []
+
     ##########
     ## Example
     ##########
@@ -28,7 +39,7 @@ def answer():
 
     key_list, music_list = get_moonjae()  # Do not change this line
     # ///////////////////////////////////////////////////////////////////////////////////
-
+    
     '''
         이 부분에 여러분의 알고리즘 구현이 들어갑니다.
         Music, Singer Class의 __init__함수를 구현하시고
